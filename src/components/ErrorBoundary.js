a976a39defea49670a5c826105d14fb1f7f2a270
kscheltema <k87scheltema@outlook.com>
2022-08-05 13:00:03 +0200

    error handling Issue importing createRoot from react-dom/client

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const ErrorBoundary = () => {
+const ErrorBoundary = (props) => {
   const [error, setError] = React.useState(null);
 
   // class ErrorBoundary extends Component {
