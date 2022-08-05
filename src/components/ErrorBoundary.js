2c9e01a2db98aecacd6f88625433f2678f16193b
kscheltema <k87scheltema@outlook.com>
2022-08-05 13:00:23 +0200

    cleaning

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -3,28 +3,10 @@ import * as React from "react";
 const ErrorBoundary = (props) => {
   const [error, setError] = React.useState(null);
 
-  // class ErrorBoundary extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       hasError: false,
-  //     };
-  //   }
-
-  // componentDidCatch(error, info) {
-  //   setHasError(true);
-  // };
-
   if (error) {
     throw error;
   }
   return props.children;
-  // render() {
-  //   if (this.state.hasError) {
-  //     return <h1>Oops. This is not good.</h1>;
-  //   }
-  //   return this.props.children;
-  // }
 };
 
 export default ErrorBoundary;
